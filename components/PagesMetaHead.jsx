ce83a1d8eed678e3a1bcf4f1c14e51fad079049a
mhuwaimel <alhuwaimel.m@outlook.sa>
2024-10-17 14:54:06 +0300

    seo-Googleverification

--- components/PagesMetaHead.jsx
+++ components/PagesMetaHead.jsx
@@ -43,7 +43,7 @@ PagesMetaHead.defaultProps = {
     "&#x645;&#x62D;&#x645;&#x62F; &#x627;&#x644;&#x647;&#x648;&#x64A;&#x645;&#x644;",
   ogUrl: "https://www.mohammedh.sa", // Replace with your URL
   ogImageUrl: "/public/hero.jpg", // Replace with your image URL
-  verification: "WVx7eEsjASR60PqML6j_K3wSyROLR5qqp890",
+  verification: "WVx7eEsjASR60PqML6j_K3wSyROLR5qqp8905y-Zv8Y",
 };
 
 export default PagesMetaHead;
